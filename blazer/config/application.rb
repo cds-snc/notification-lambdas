27d24cfb92c952f06c93c8238470beebb1f5db5b
Pat Heard <patrick.heard@cds-snc.ca>
2022-11-16 11:24:50 -0500

    feat: initial commit of Blazer Docker setup
    Add the Blazer Docker image that is configured to use
    Google OAuth2 for sign in.

---
+++ blazer/config/application.rb
@@ -0,0 +1,56 @@
+require "bundler/setup"
+Bundler.require
+
+require "rails"
+
+%w(
+  active_record/railtie
+  action_controller/railtie
+  action_mailer/railtie
+  action_view/railtie
+  sprockets/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
+
+abort "No DATABASE_URL" unless ENV["DATABASE_URL"]
+
+module BlazerSolo
+  class Application < Rails::Application
+    routes.append do
+      # checks app is up and can connect to database
+      # does not check data sources
+      # not protected by auth, so do not expose data
+      get "health", to: ->(env) {
+                      if Blazer::Connection.connection.active?
+                        [200, {}, ["OK"]]
+                      else
+                        [503, {}, ["Service Unavailable"]]
+                      end
+                    }
+
+      devise_for :users, controllers: {
+                           omniauth_callbacks: "users/omniauth_callbacks",
+                         }
+
+      authenticate :user, ->(user) { user.present? } do
+        mount Blazer::Engine, at: "/"
+      end
+    end
+
+    config.cache_classes = true
+    config.eager_load = true
+    config.log_level = ENV["LOG_LEVEL"].present? ? ENV["LOG_LEVEL"].to_sym : :warn
+    config.secret_key_base = ENV["SECRET_KEY_BASE"] || SecureRandom.hex(30)
+    config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"] != "disabled"
+
+    if ENV["RAILS_LOG_TO_STDOUT"] != "disabled"
+      logger = ActiveSupport::Logger.new(STDOUT)
+      logger.formatter = config.log_formatter
+      config.logger = ActiveSupport::TaggedLogging.new(logger)
+    end
+  end
+end
