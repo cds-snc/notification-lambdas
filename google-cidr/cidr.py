10952ad7246a93666ce79622bdd828f24d293cff
Pat Heard <patrick.heard@cds-snc.ca>
2022-11-17 15:13:29 -0500

    feat: add Google CIDR tests and fix lint errors (#3)
    Add unit tests, fix lint errors and add a devcontainer for the
    Google CIDR lambda code.

--- google-cidr/cidr.py
+++ google-cidr/cidr.py
@@ -1,19 +1,18 @@
 """
 Updates a customer managed VPC prefix list with the valid Google service CIDR
-ranges.  These are determined by retrieving Google"s service CIDR range and 
+ranges.  These are determined by retrieving Google"s service CIDR range and
 subtracting the Google Cloud Platform CIDR ranges.
 
 Adapted from Google"s CIDR tool:
 https://github.com/GoogleCloudPlatform/networking-tools-python/tree/main/tools/cidr
 """
-import boto3
 import json
 import logging
-
 from os import environ
 from urllib.request import urlopen
 from urllib.error import HTTPError
 
+import boto3
 
 IPRANGE_URLS = {
     "goog": environ.get(
@@ -32,27 +31,30 @@ def read_url(url):
     try:
         return json.loads(urlopen(url).read())
     except (IOError, HTTPError):
-        logging.error(f"ERROR: Invalid HTTP response from {url}")
+        logging.error("ERROR: Invalid HTTP response from %s", url)
     except json.decoder.JSONDecodeError:
-        logging.error(f"ERROR: Could not parse HTTP response from {url}")
+        logging.error("ERROR: Could not parse HTTP response from %s", url)
+
+    return {}
 
 
 def get_data(link):
     """Returns a list of CIDRs from a Google IP ranges JSON object."""
+    cidrs = set([])
     data = read_url(link)
+
     if data:
-        logging.info(f"{link} published: {data.get('creationTime')}")
-        cidrs = set([])
-        for e in data["prefixes"]:
-            if "ipv4Prefix" in e:
-                cidrs.add(e.get("ipv4Prefix"))
-        return cidrs
+        logging.info("%s published: %s", link, data.get("creationTime"))
+        for elem in data["prefixes"]:
+            if "ipv4Prefix" in elem:
+                cidrs.add(elem.get("ipv4Prefix"))
+
+    return cidrs
 
 
 def get_google_cidrs(iprange_urls):
+    """Returns a list of Google service CIDR ranges"""
     cidrs = {group: get_data(link) for group, link in iprange_urls.items()}
-    if len(cidrs) != 2:
-        raise ValueError("ERROR: Could not process data from Google")
     return cidrs["goog"] - cidrs["cloud"]
 
 
@@ -77,12 +79,13 @@ def get_prefix_list_entries(ec2_client, prefix_list_id, version):
 def modify_managed_prefix_list(
     ec2_client, prefix_list_id, version, google_cidrs, entries
 ):
+    """Updates the prefix list with the Google service CIDR ranges"""
     entries_to_add = set(google_cidrs) - set(entries)
     entries_to_remove = set(entries) - set(google_cidrs)
     if entries_to_add or entries_to_remove:
-        logging.info(f"Adding entries: {entries_to_add}")
-        logging.info(f"Removing entries: {entries_to_add}")
-        response = ec2_client.modify_managed_prefix_list(
+        logging.info("Adding entries: %s", entries_to_add)
+        logging.info("Removing entries: %s", entries_to_remove)
+        ec2_client.modify_managed_prefix_list(
             PrefixListId=prefix_list_id,
             CurrentVersion=version,
             AddEntries=[{"Cidr": e} for e in entries_to_add],
@@ -92,14 +95,18 @@ def modify_managed_prefix_list(
         logging.info("No changes to prefix list")
 
 
-def get_boto_client(type):
+def get_boto_client(client_type):
     """Returns a boto3 client of the given type"""
-    return boto3.client(type)
+    return boto3.client(client_type)
 
 
-def handler(event, context):
+def handler():
     """Retrieves the list of public Google service CIDR ranges"""
     google_cidrs = get_google_cidrs(IPRANGE_URLS)
+
+    if len(google_cidrs) == 0:
+        logging.error("ERROR: No Google service CIDR ranges found")
+        return {"statusCode": 404, "body": "No Google CIDRs found"}
 
     ec2_client = get_boto_client("ec2")
     version = get_prefix_list_version(ec2_client, PREFIX_LIST_ID)
@@ -108,7 +115,7 @@ def handler(event, context):
         ec2_client, PREFIX_LIST_ID, version, google_cidrs, entries
     )
 
-    return "success"
+    return {"statusCode": 200, "body": "Prefix list updated"}
 
 
 if __name__ == "__main__":
--- google-cidr/test_cidr.py
+++ google-cidr/test_cidr.py
@@ -1,8 +1,14 @@
 import pytest
 import cidr
-import sys
 
 from unittest.mock import call, MagicMock, patch
+
+
+@patch("cidr.urlopen")
+def test_read_url(mock_urlopen):
+    mock_urlopen.return_value.read.return_value = b'{"foo": "bar"}'
+    assert cidr.read_url("http://example.com") == {"foo": "bar"}
+    mock_urlopen.assert_called_once_with("http://example.com")
 
 
 @patch("cidr.read_url")
@@ -69,6 +75,93 @@ def test_get_google_cidrs(mock_read_url):
             call("https://cloud.com"),
         ]
     )
+
+
+def test_get_prefix_list_version():
+    ec2_client = MagicMock(return_value={"PrefixLists": [{"Version": 1}]})
+    ec2_client.describe_managed_prefix_lists.return_value = {
+        "PrefixLists": [{"Version": 42}]
+    }
+    assert cidr.get_prefix_list_version(ec2_client, "pl-12345678") == 42
+    ec2_client.describe_managed_prefix_lists.assert_called_once_with(
+        PrefixListIds=["pl-12345678"]
+    )
+
+
+def test_get_prefix_list_entries():
+    ec2_client = MagicMock()
+    ec2_client.get_managed_prefix_list_entries.return_value = {
+        "Entries": [
+            {
+                "Cidr": "1.2.3.4/32",
+            },
+            {
+                "Cidr": "66.55.44.33/32",
+            },
+        ]
+    }
+    assert cidr.get_prefix_list_entries(ec2_client, "pl-12345678", "42") == [
+        "1.2.3.4/32",
+        "66.55.44.33/32",
+    ]
+    ec2_client.get_managed_prefix_list_entries.assert_called_once_with(
+        MaxResults=100, PrefixListId="pl-12345678", TargetVersion="42"
+    )
+
+
+def test_modify_managed_prefix_list():
+    ec2_client = MagicMock()
+    ec2_client.modify_managed_prefix_list.return_value = "success"
+    cidr.modify_managed_prefix_list(
+        ec2_client,
+        "pl-12345678",
+        "42",
+        ["9.8.7.6/32", "8.7.6.5/16"],
+        ["9.8.7.6/32", "1.2.3.4/24"],
+    )
+
+    ec2_client.modify_managed_prefix_list.assert_called_once_with(
+        PrefixListId="pl-12345678",
+        CurrentVersion="42",
+        AddEntries=[{"Cidr": "8.7.6.5/16"}],
+        RemoveEntries=[{"Cidr": "1.2.3.4/24"}],
+    )
+
+
+@patch("cidr.get_google_cidrs")
+@patch("cidr.get_boto_client")
+@patch("cidr.get_prefix_list_version")
+@patch("cidr.get_prefix_list_entries")
+@patch("cidr.modify_managed_prefix_list")
+@patch("cidr.PREFIX_LIST_ID", "pl-1234567890")
+def test_handler(
+    mock_modify_managed_prefix_list,
+    mock_get_prefix_list_entries,
+    mock_get_prefix_list_version,
+    mock_get_boto_client,
+    mock_get_google_cidrs,
+):
+    mock_get_google_cidrs.return_value = set(["1.2.3.4/32"])
+    mock_get_boto_client.return_value = "client"
+    mock_get_prefix_list_version.return_value = "42"
+    mock_get_prefix_list_entries.return_value = ["2.3.4.5/32"]
+
+    assert cidr.handler() == {"statusCode": 200, "body": "Prefix list updated"}
+
+    mock_get_boto_client.assert_called_once_with("ec2")
+    mock_get_prefix_list_version.assert_called_once_with("client", "pl-1234567890")
+    mock_get_prefix_list_entries.assert_called_once_with(
+        "client", "pl-1234567890", "42"
+    )
+    mock_modify_managed_prefix_list.assert_called_once_with(
+        "client", "pl-1234567890", "42", set(["1.2.3.4/32"]), ["2.3.4.5/32"]
+    )
+
+
+@patch("cidr.get_google_cidrs")
+def test_handler_no_cidrs(mock_get_google_cidrs):
+    mock_get_google_cidrs.return_value = set()
+    assert cidr.handler() == {"statusCode": 404, "body": "No Google CIDRs found"}
 
 
 @patch("cidr.boto3")
