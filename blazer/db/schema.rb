00174bb174e5ab0f6e8d0d45cb49bb447048078a
Pat Heard <patrick.heard@cds-snc.ca>
2022-11-19 17:01:12 +0000

    feat: add controller request spec

--- blazer/db/schema.rb
+++ blazer/db/schema.rb
@@ -77,12 +77,9 @@ ActiveRecord::Schema.define(version: 2022_11_09_194810) do
     t.string "uid"
     t.string "avatar_url"
     t.string "provider"
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
