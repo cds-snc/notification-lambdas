1b93a96e109a16a7c4cbe3afd808a68ca829616f
Jumana Bahrainwala <jzbahrai@uwaterloo.ca>
2026-09-18 13:55:30 -0400

    fixes

--- blazer/config/initializers/blazer_check_source_guard.rb
+++ blazer/config/initializers/blazer_check_source_guard.rb
@@ -1,5 +1,13 @@
 # Restrict checks to queries that use the dedicated checks data source.
 module BlazerChecksDataSourceGuard
+  # Direct calls to Blazer.run_check must not evaluate non-check datasources.
+  def run_check(check)
+    checks_data_source = ENV.fetch("BLAZER_CHECKS_DATA_SOURCE_NAME", "checks")
+    return unless check.query&.data_source == checks_data_source
+
+    super
+  end
+
   def run_checks(schedule: nil)
     checks_data_source = ENV.fetch("BLAZER_CHECKS_DATA_SOURCE_NAME", "checks")
 
@@ -14,22 +22,13 @@ module BlazerChecksDataSourceGuard
   end
 end
 
+# Defense in depth: block state updates even if run_check is reached some other way.
 module BlazerChecksExecutionGuard
-  def perform(statement, options = {})
-    result = super
-    query = options[:query]
-    return result unless query && !result.timed_out? && !result.cached? && !query.variables.any?
-
+  def update_state(result)
     checks_data_source = ENV.fetch("BLAZER_CHECKS_DATA_SOURCE_NAME", "checks")
-    return result if query.data_source == checks_data_source
+    return unless query&.data_source == checks_data_source
 
-    query.checks.each do |check|
-      next unless check.query&.data_source == checks_data_source
-
-      check.update_state(result)
-    end
-
-    result
+    super
   end
 end
 
@@ -42,31 +41,50 @@ Rails.application.config.to_prepare do
 
       def query_must_use_checks_data_source
         return if query.blank?
-        checks_data_source = ENV.fetch("BLAZER_CHECKS_DATA_SOURCE_NAME", "checks")
-        return if query.data_source == checks_data_source
 
-        errors.add(:base, "Checks can only be created for queries using the #{checks_data_source} data source")
+        validate_check_query_data_source do
+          checks_data_source = ENV.fetch("BLAZER_CHECKS_DATA_SOURCE_NAME", "checks")
+          return if query.data_source == checks_data_source
+
+          errors.add(:base, "Checks can only use queries configured with the #{checks_data_source} data source")
+        end
+      end
+
+      # Lock the persisted query row to avoid racing a concurrent datasource change.
+      def validate_check_query_data_source
+        return yield unless query.persisted?
+
+        query.with_lock { yield }
       end
     end
   end
 
   unless Blazer::Query.method_defined?(:query_with_checks_must_use_checks_data_source)
     Blazer::Query.class_eval do
-      validate :query_with_checks_must_use_checks_data_source, if: :data_source_changed?
+      validate :query_with_checks_must_use_checks_data_source
 
       private
 
       def query_with_checks_must_use_checks_data_source
-        return unless checks.exists?
+        validate_checks_data_source do
+          return unless checks.any?
 
-        checks_data_source = ENV.fetch("BLAZER_CHECKS_DATA_SOURCE_NAME", "checks")
-        return if data_source == checks_data_source
+          checks_data_source = ENV.fetch("BLAZER_CHECKS_DATA_SOURCE_NAME", "checks")
+          return if data_source == checks_data_source
 
-        errors.add(:base, "Queries with checks must use the #{checks_data_source} data source")
+          errors.add(:base, "Queries with checks must use the #{checks_data_source} data source")
+        end
+      end
+
+      # Lock this query row to avoid racing a concurrent check creation.
+      def validate_checks_data_source
+        return yield unless persisted?
+
+        with_lock { yield }
       end
     end
   end
 
   Blazer.singleton_class.prepend(BlazerChecksDataSourceGuard) unless Blazer.singleton_class < BlazerChecksDataSourceGuard
-  Blazer::RunStatement.prepend(BlazerChecksExecutionGuard) unless Blazer::RunStatement < BlazerChecksExecutionGuard
+  Blazer::Check.prepend(BlazerChecksExecutionGuard) unless Blazer::Check < BlazerChecksExecutionGuard
 end
